4bfdfa491fff09845f3968ddd6515adb6a3b9f66
Nicholas-Baron <njbaron@cpp.edu>
2019-08-02 17:54:06 -0700

    Invokes the new build system

--- tools/build.sh
+++ tools/build.sh
@@ -4,10 +4,10 @@ OUTPUT_DIR="build"
 
 if [ -d $OUTPUT_DIR ]; then rm -Rf $OUTPUT_DIR/; fi
 
-mkdir -p $OUTPUT_DIR
-cp src/* $OUTPUT_DIR/
+mkdir -p $OUTPUT_DIR temp
 cd $OUTPUT_DIR || exit
 
 pwd
 
+cmake ..
 make -j
