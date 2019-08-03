c3793c291556ba7d6cd515668dc440bb6497eb86
Nicholas-Baron <njbaron@cpp.edu>
2019-08-02 20:33:56 -0700

    Added a build log output

--- tools/build.sh
+++ tools/build.sh
@@ -4,10 +4,12 @@ OUTPUT_DIR="build"
 
 if [ -d $OUTPUT_DIR ]; then rm -Rf $OUTPUT_DIR/; fi
 
-mkdir -p $OUTPUT_DIR temp
+mkdir -p $OUTPUT_DIR temp logs
 cd $OUTPUT_DIR || exit
 
 pwd
 
-cmake ..
-make -j
+(
+	cmake ..
+	make -j
+) 2>&1 | tee ../logs/build.txt
