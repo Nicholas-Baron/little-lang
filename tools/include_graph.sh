7f2d2303ed2ad949df7afe130b241b0780265d5b
Nicholas-Baron <njbaron@cpp.edu>
2019-08-10 00:37:25 -0700

    Added the C files to the include graph

--- tools/include_graph.sh
+++ tools/include_graph.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-tools/external/cinclude2dot --src src/ --include temp/,/usr/lib/llvm-8/include,/usr/include/c++/8 | dot -Tpdf > temp/includes.pdf
+tools/external/cinclude2dot --src src/ --include temp/,/usr/lib/llvm-8/include,/usr/include/c++/8 | dot -Tpdf -o temp/includes_src.pdf
+tools/external/cinclude2dot --src temp/ --include src/,/usr/lib/llvm-8/include,/usr/include/c++/8,/usr/include | dot -Tpdf -o temp/includes_temp.pdf
