9fad3007ee04548d02b42df7173eac87b12e0c89
Nicholas-Baron <njbaron@cpp.edu>
2019-08-10 11:45:35 -0700

    Attempted to prettify the dot graphs

--- tools/include_graph.sh
+++ tools/include_graph.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tools/external/cinclude2dot --src src/ --include temp/,/usr/lib/llvm-8/include,/usr/include/c++/8 | dot -Tpdf -o temp/includes_src.pdf
-tools/external/cinclude2dot --src temp/ --include src/,/usr/lib/llvm-8/include,/usr/include/c++/8,/usr/include | dot -Tpdf -o temp/includes_temp.pdf
+tools/external/cinclude2dot --src src/ --include external/,temp/,/usr/lib/llvm-8/include,/usr/include/c++/8 | unflatten -l5 -f | dot -Tpdf -o temp/includes_src.pdf
+tools/external/cinclude2dot --src temp/ --include src/,/usr/lib/llvm-8/include,/usr/include/c++/8,/usr/include | unflatten -l5 -f | dot -Tpdf -o temp/includes_temp.pdf
