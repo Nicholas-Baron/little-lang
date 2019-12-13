bee99c0a0b3b0efd47b0d8a4113a5c588f3d19dd
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-12-12 17:43:52 -0800

    There is now a testing log

--- tools/test.sh
+++ tools/test.sh
@@ -4,9 +4,11 @@ COMPILER="build/littlec"
 
 [ -f $COMPILER ] || tools/build.sh || exit
 
-for file in ./test/*; do
-	echo "Testing $file..."
-	/usr/bin/time -v $COMPILER "$file"
-	printf "\n"
-done
+(
+	for file in ./test/*; do
+		echo "Testing $file..."
+		/usr/bin/time -v $COMPILER "$file"
+		printf "\n"
+	done
+) 2>&1 | tee ./logs/testing.txt
 
