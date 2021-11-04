51fe1ff19f069d9cbc18ad36dc0c7cf43c8a8b8f
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-11-04 00:22:22 -0700

    Update parser_graph script

--- tools/parser_graph.sh
+++ tools/parser_graph.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-cd temp || exit
+cd build/temp || exit
 
 echo "Flattening the parser graph..."
-unflatten -l10 -f -c 5 -o flattened.dot < parser.dot
+unflatten -l10 -f -c 5 -o flattened.dot < parser.gv
 
 echo "Drawing parser graph..."
-time dot -Tpdf flattened.dot -o parser.pdf
+time dot -Tsvg flattened.dot -o parser.svg
 
+chrome parser.svg >/dev/null 2>&1 &
