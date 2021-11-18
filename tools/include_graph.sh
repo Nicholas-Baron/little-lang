5197efa2a3f09b57080f713eb58727a182df39d5
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-11-17 17:54:46 -0800

    Updated include_graph to act more like CMake

--- tools/include_graph.sh
+++ tools/include_graph.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 llvm_flags=$(llvm-config --cxxflags --ldflags --libs core native | grep -o '/[^ ]*' | grep 'include')
 
 touch lol.cpp
@@ -10,5 +9,7 @@ rm -f lol.o lol.cpp &
 # echo "LLVM: $llvm_flags"
 # echo "Clang: $cpp_lib"
 
-tools/external/cinclude2dot --src src/ --include build/temp/,$llvm_flags,$cpp_lib | dot -Tsvg -o includes.svg
+include_paths="build/temp/,src/,$llvm_flags,$cpp_lib"
+
+tools/external/cinclude2dot --src src/ --include $include_paths | dot -Tsvg -o includes.svg
 chrome includes.svg 2>&1 >/dev/null &
