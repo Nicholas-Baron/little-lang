d8ad579f0636f99f2d5651045e57cae3f55cbdb2
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-11-22 13:06:06 -0800

    Added asserts in value_getter

--- src/visitor/value_getter.hpp
+++ src/visitor/value_getter.hpp
@@ -4,6 +4,9 @@
 #include "visitor_base.hpp"
 #include <type_traits>
 
+#include <cassert>
+#include <optional>
+
 namespace visitor {
     // NOTE: visitor_impl cannot be tested, as it is incomplete at the time of instantiation.
     template<typename visitor_impl, typename visitable, typename result_t>
@@ -12,14 +15,20 @@ namespace visitor {
         static_assert(std::is_class_v<visitable>, "we must visit classes");
         [[nodiscard]] static result_t get_value(visitable & n, visitor_impl & context) {
             n.accept(context);
-            return context.result;
+            assert(context.result.has_value());
+            auto value = std::move(context.result).value();
+            context.result.reset();
+            return value;
         }
 
         static_assert(not std::is_reference_v<result_t>, "we can only return non-references");
-        void store_result(result_t value) { result = value; }
+        void store_result(result_t value) {
+            assert(not result.has_value());
+            result = value;
+        }
 
       private:
-        result_t result;
+        std::optional<result_t> result;
     };
 
 } // namespace visitor
