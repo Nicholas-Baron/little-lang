dff2e39e2ecfa74b843de719fabcd592905c7b7c
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-12-11 20:02:40 -0800

    Add `_num` to disambiguate from C's stdlib

--- tools/syscall_gen.sh
+++ tools/syscall_gen.sh
@@ -8,6 +8,6 @@ mkdir -p examples/stdlib
 	echo "# extracted from $syscall_source"
 	echo "export {"
 	grep '^#define __' $syscall_source \
-		| sed -E 's/#define __NR_([a-z0-9_]+) ([0-9]+)/    const \1 : int = \2/g'
+		| sed -E 's/#define __NR_([a-z0-9_]+) ([0-9]+)/    const \1_num : int = \2/g'
 	echo "}"
 } > examples/stdlib/syscall_nums.lil
