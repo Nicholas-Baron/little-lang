b238ef94651c5d1b806ea5f985ef97d8e1190cd6
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-12-11 18:42:07 -0800

    Add generator script for syscall numbers

---
+++ tools/syscall_gen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+syscall_source='/usr/include/asm/unistd_64.h'
+
+mkdir -p examples/stdlib
+
+{
+	echo "# extracted from $syscall_source"
+	echo "export {"
+	grep '^#define __' $syscall_source \
+		| sed -E 's/#define __NR_([a-z0-9_]+) ([0-9]+)/    const \1 : int = \2/g'
+	echo "}"
+} > examples/stdlib/syscall_nums.lil
