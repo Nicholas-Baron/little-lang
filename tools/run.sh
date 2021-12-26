418558c998134edf60629a86e59e2a4fa954087b
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-12-26 14:51:08 -0800

    Removed old scripts

--- tools/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-[ $# -ne 1 ] && echo "Usage: $0 <input file>" && exit
-
-[ -f "build/littlec" ] || tools/build.sh || exit
-
-build/littlec $1 | llc-8 - --x86-asm-syntax=intel -O=2 -filetype=obj -o "$1.o"
-
