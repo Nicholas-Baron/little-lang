0d51d34d964b26c09823bab5b8fb80b2232470d9
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-12-26 15:18:01 -0800

    Add a script to test all examples with main

---
+++ tools/test_examples.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Setup the build folder
+[ -d build ] || cmake -B build -DCMAKE_BUILD_TYPE=Debug -GNinja . || exit
+
+compiler="$(realpath -m build/bin/littlec)"
+examples_dir=$(realpath -e examples)
+
+# Build the compiler
+[ -x "$compiler" ] || ninja littlec
+[ -x "$compiler" ] || exit
+
+# Setup a temp dir
+old_dir=$PWD
+active_dir=$(mktemp -d)
+cd $active_dir || exit
+
+# Compile examples that contain the word `main`
+for f in $examples_dir/*.lil; do
+	if [ ! -f "$f" ]; then
+		continue
+	elif ! grep -wq main $f; then
+		continue
+	fi
+
+	echo "Compiling $f ..."
+	$compiler $f
+	success=$?
+	if [ $success -ne 0 ]; then
+		echo "Failed to compile $f : exited with $success"
+	fi
+done
+
+# Cleanup
+cd $old_dir
+rm -rf $active_dir
