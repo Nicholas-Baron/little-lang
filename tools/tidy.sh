5af47823c221ebf00544f5afec0b73f5fea56755
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-02-02 17:11:35 -0800

    Print out summary of tidy results

--- tools/tidy.sh
+++ tools/tidy.sh
@@ -5,3 +5,6 @@ flags=$(jq '.[]["command"]' compile_commands.json | sed 's:"/usr/bin/clang++ \(.
 echo "Flags: $flags"
 echo "Beginning linting..."
 clang-tidy src/*.cpp src/*.hpp -- $flags 2>&1 | tee tidy.txt
+
+echo "Summary"
+grep '[a-z_]+\.(c|h)pp' tidy.txt -Eo | sort | uniq -c | sort -rn
