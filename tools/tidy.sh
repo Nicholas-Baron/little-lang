42c44822c0846e164b3e7949af19e8e2ecd5068a
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-02-28 14:45:09 -0800

    Use git ls-files to get all source files

--- tools/tidy.sh
+++ tools/tidy.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 flags=$(jq '.[]["command"]' compile_commands.json | sed 's:"/usr/bin/clang++ \(.*\) -o.*:\1:g' | head -n1)
+files=$(git ls-files -- 'src/*.cpp' 'src/*.hpp')
 
 echo "Flags: $flags"
 echo "Beginning linting..."
-clang-tidy src/*.cpp src/*.hpp -- $flags 2>&1 | tee tidy.txt
+clang-tidy $files -- $flags 2>&1 | tee tidy.txt
 
 echo "Summary"
 grep '[a-z_]+\.(c|h)pp' tidy.txt -Eo | sort | uniq -c | sort -rn | tee -a tidy.txt
