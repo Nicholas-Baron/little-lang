fd931f20197253e725f3d55c8debca6dbc035292
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-03-04 15:52:19 -0800

    Automatically save the old build profile

--- tools/build_profile.sh
+++ tools/build_profile.sh
@@ -10,6 +10,10 @@ cd build
 ninja clean
 ninja
 
+[ -f trace.json ] && mv -vb trace.json old_trace.json
+
 ../tools/external/ninjatracing .ninja_log > trace.json
+
+[ -f profile.txt ] && mv -vb profile.txt old_profile.txt
 
 grep -Eo 'name[^{}]+dur": [0-9]+' trace.json | sed 's/^name": "\([^"]*\)".*: \([0-9]*\)$/\2 : \1/' | sort -rn > profile.txt
