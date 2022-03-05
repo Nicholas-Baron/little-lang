2c36c518777d34b91c7dde3d703f07249805c70b
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-03-04 18:51:43 -0800

    Improve tidy script
    1. Move output away from stdout and into the file
    2. Improve the summary counts

--- tools/tidy.sh
+++ tools/tidy.sh
@@ -2,10 +2,11 @@
 
 flags=$(jq '.[]["command"]' compile_commands.json | sed 's:"/usr/bin/clang++ \(.*\) -o.*:\1:g' | head -n1)
 files=$(git ls-files -- 'src/*.cpp' 'src/*.hpp')
+clang-tidy --quiet $files -- $flags 2>&1 > tidy.txt
 
-echo "Flags: $flags"
-echo "Beginning linting..."
-clang-tidy --quiet $files -- $flags 2>&1 | tee tidy.txt
+{
+	echo "Summary"
+	grep '[A-Za-z_]+\.(c|h)pp:[0-9:]+ warning' tidy.txt -Eo | cut -f1 -d':' \
+		| sort | uniq -c | sort -rn
 
-echo "Summary"
-grep '[a-z_]+\.(c|h)pp' tidy.txt -Eo | sort | uniq -c | sort -rn | tee -a tidy.txt
+} | tee -a tidy.txt
