5f3b5cc1f3df341a60bc02389ac78463da80e690
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-09-13 14:44:37 -0700

    Add six basic nodes and connect them to the base visitor

---
+++ src/control_flow/graph.cpp
@@ -0,0 +1,9 @@
+#include "graph.hpp"
+
+#include "node.hpp"
+
+namespace control_flow {
+    graph::graph() = default;
+
+    graph::~graph() noexcept = default;
+} // namespace control_flow
--- src/control_flow/visitor.hpp
+++ src/control_flow/visitor.hpp
@@ -2,10 +2,22 @@
 #ifndef VISITOR_HPP
 #define VISITOR_HPP
 
+#include "node_forward.hpp"
+
+#include <move_copy.hpp>
+
 namespace control_flow {
 
     class visitor {
       public:
+        virtual ~visitor() noexcept = default;
+
+        non_copyable(visitor);
+        movable(visitor);
+
+#define expand_node_macro(x) virtual void visit(x &) = 0;
+        all_cfg_nodes
+#undef expand_node_macro
     };
 
 } // namespace control_flow
