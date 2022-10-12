b0d14373eb80741d8a5410031093db7f456a16ea
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-10-12 01:37:26 -0700

    Add a debug dump function for control_flow graph

--- src/control_flow/graph.cpp
+++ src/control_flow/graph.cpp
@@ -2,8 +2,16 @@
 
 #include "node.hpp"
 
+#include <iomanip>  // hex
+#include <iostream> // cout
+
 namespace control_flow {
     graph::graph() = default;
 
     graph::~graph() noexcept = default;
+
+    void graph::list_all_nodes() const noexcept {
+        std::cout << std::hex;
+        for (const auto & node : nodes) { std::cout << static_cast<void *>(node.get()) << '\n'; }
+    }
 } // namespace control_flow
