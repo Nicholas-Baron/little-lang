ebd4097900ead5185d977a79b3aac6fb3b9a38f4
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-10-12 01:37:58 -0700

    Start linking the next pointers correctly
    The `flows_from` function on node only handles the previous pointers, so
    we need to traverse the graph and build the next pointers.

--- test/ast_to_cfg_test.cpp
+++ test/ast_to_cfg_test.cpp
@@ -1,6 +1,7 @@
 #include "ast/nodes.hpp"
 #include "ast/parser.hpp"
 #include "ast_to_cfg.hpp"
+#include "control_flow/node.hpp"
 
 #include <catch2/catch.hpp>
 
@@ -24,8 +25,29 @@ TEST_CASE("ast_to_cfg can lower an empty function") {
     auto cfg = std::move(lowering).take_cfg();
     CHECK(cfg != nullptr);
 
+    // There should be 1 root.
+    const control_flow::function_start * start = nullptr;
+    auto root_count = 0U;
+    cfg->for_each_root([&](auto * root) {
+        CHECK(root != nullptr);
+        ++root_count;
+
+        const auto * func_start = dynamic_cast<const control_flow::function_start *>(root);
+        CHECK(func_start != nullptr);
+        start = func_start;
+        CHECK(func_start->arg_count == 0);
+
+        // Note that even though LLVM requires main to be exported,
+        // at the language level, it is not.
+        CHECK_FALSE(func_start->exported);
+        CHECK(func_start->next != nullptr);
+    });
+    CHECK(root_count == 1);
+
     // The last node generated should be a function end.
     CHECK(cfg->previous_node() != nullptr);
     auto * func_end = dynamic_cast<control_flow::function_end *>(cfg->previous_node());
     CHECK(func_end != nullptr);
+    CHECK(start->next == func_end);
+    CHECK(func_end->previous == start);
 }
