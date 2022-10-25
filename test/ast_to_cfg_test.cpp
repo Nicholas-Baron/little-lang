26f117ddc9ae1bd106461455ab0cd209c9458724
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-10-25 16:01:20 -0700

    Test short circuiting

--- test/ast_to_cfg_test.cpp
+++ test/ast_to_cfg_test.cpp
@@ -3,6 +3,9 @@
 #include "ast_to_cfg.hpp"
 #include "control_flow/node.hpp"
 
+#include <iostream>
+#include <queue>
+
 #include <catch2/catch.hpp>
 
 TEST_CASE("ast_to_cfg is initially empty") {
@@ -50,4 +53,75 @@ TEST_CASE("ast_to_cfg can lower an empty function") {
     CHECK(func_end != nullptr);
     CHECK(start->next == func_end);
     CHECK(func_end->previous == start);
+}
+
+TEST_CASE("ast_to_cfg can lower a shortcircuiting expression") {
+    std::string buffer = "positive_even(x : int) = x > 0 and x % 2 == 0";
+    auto parser = parser::from_buffer(buffer);
+    auto mod = parser->parse();
+
+    auto lowering = ast_to_cfg{};
+
+    lowering.visit(*mod);
+
+    auto cfg = std::move(lowering).take_cfg();
+    CHECK(cfg != nullptr);
+
+    // There should be 1 root.
+    const control_flow::function_start * start = nullptr;
+    auto root_count = 0U;
+    cfg->for_each_root([&](auto * root) {
+        CHECK(root != nullptr);
+        ++root_count;
+
+        const auto * func_start = dynamic_cast<const control_flow::function_start *>(root);
+        CHECK(func_start != nullptr);
+        start = func_start;
+        CHECK(func_start->arg_count == 1);
+
+        CHECK_FALSE(func_start->exported);
+        CHECK(func_start->next != nullptr);
+    });
+    CHECK(root_count == 1);
+
+    // The last node generated should be a function end.
+    CHECK(cfg->previous_node() != nullptr);
+    auto * func_end = dynamic_cast<control_flow::function_end *>(cfg->previous_node());
+    CHECK(func_end != nullptr);
+
+    std::queue<const control_flow::node *> frontier;
+    frontier.push(start->next);
+    auto found_end = false;
+
+    while (not frontier.empty()) {
+        const auto * visiting = frontier.front();
+        CHECK(visiting != nullptr);
+
+        std::cout << "Visiting " << typeid(*visiting).name() << " @ "
+                  << static_cast<const void *>(visiting) << std::endl;
+
+        if (visiting == func_end) {
+            found_end = true;
+        } else if (const auto * bin_op
+                   = dynamic_cast<const control_flow::binary_operation *>(visiting);
+                   bin_op != nullptr) {
+            CHECK(bin_op->next != nullptr);
+            frontier.push(bin_op->next);
+        } else if (const auto * branch = dynamic_cast<const control_flow::branch *>(visiting);
+                   branch != nullptr) {
+            frontier.push(branch->true_case);
+            frontier.push(branch->false_case);
+        } else if (const auto * value = dynamic_cast<const control_flow::constant *>(visiting);
+                   value != nullptr) {
+            CHECK(value->next != nullptr);
+            frontier.push(value->next);
+        } else {
+            std::cout << typeid(*visiting).name() << std::endl;
+            assert(false);
+        }
+
+        frontier.pop();
+    }
+
+    CHECK(found_end);
 }
