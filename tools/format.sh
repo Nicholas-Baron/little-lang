a89e6fc7400467b2f5e9933b019130e63557ff74
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-11-10 17:00:54 -0800

    Use `find` for finding header and source files

--- tools/format.sh
+++ tools/format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-clang-format $(git ls-files -- *.cpp *.hpp) -i -verbose -style=file -fallback-style=none
+clang-format $(find src -type f -name '*.[ch]pp' | tr '\n' ' ') -i -verbose -style=file -fallback-style=none
 cmake-format $(find . -type f -name 'CMakeLists.txt' | tr '\n' ' ') -i -c .cmake-format
 
 git diff
