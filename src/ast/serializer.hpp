6506b2ff57ef6cfd7b9f706e1f035184ed8b57a6
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-11-12 15:27:07 -0800

    Start implementing AST to JSON serializer

--- src/ast/serializer.hpp
+++ src/ast/serializer.hpp
@@ -3,6 +3,8 @@
 
 #include "visitor_base.hpp"
 
+#include <iosfwd> // ostream
+
 #include <move_copy.hpp>
 #include <nlohmann/json.hpp>
 #include <value_getter.hpp>
@@ -11,11 +13,21 @@ namespace ast {
     class serializer : public visitor_base,
                        public value_getter<serializer, ast::node, nlohmann::json> {
       public:
+        explicit serializer(const std::string & filename);
         non_copyable(serializer);
         movable(serializer);
         ~serializer() noexcept override = default;
 
-      private:
+        void dump(std::ostream &, bool human_readable) const;
+
+// clang-format off
+#define expand_node_macro(name) void visit(name &) override;
+        ast_nodes
+#undef expand_node_macro
+	  private:
+            // clang-format on
+
+            nlohmann::json mod;
     };
 } // namespace ast
 
