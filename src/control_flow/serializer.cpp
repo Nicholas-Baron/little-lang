29ef839a0477e8c1c0a41f39f4ab54ac65bd5335
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-12-06 15:32:28 -0800

    Add API to serialize whole CFG

--- src/control_flow/serializer.cpp
+++ src/control_flow/serializer.cpp
@@ -4,6 +4,14 @@
 
 namespace control_flow {
 
+    void serializer::into_stream(std::ostream & stream, const std::vector<node *> & roots,
+                                 bool human_readable) {
+        serializer visitor{};
+        nlohmann::json result;
+        for (auto * node : roots) { result.push_back(serializer::get_value(*node, visitor)); }
+        stream << result.dump(human_readable ? 4 : -1);
+    }
+
     void serializer::visit(function_start & function_start) {
         return store_result(nlohmann::json::object_t{{"arg count", function_start.arg_count},
                                                      {"exported", function_start.exported}});
--- src/control_flow/serializer.hpp
+++ src/control_flow/serializer.hpp
@@ -9,16 +9,22 @@
 
 namespace control_flow {
     class serializer final : public visitor, public value_getter<serializer, node, nlohmann::json> {
-
       public:
+        static void into_stream(std::ostream &, const std::vector<node *> &, bool human_readable);
+
         ~serializer() noexcept override = default;
 
         non_copyable(serializer);
         movable(serializer);
 
+        // clang-format off
 #define expand_node_macro(x) virtual void visit(x &) override;
         all_cfg_nodes
 #undef expand_node_macro
+
+      private:
+        serializer() = default;
+        // clang-format on
     };
 } // namespace control_flow
 
