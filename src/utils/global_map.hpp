428d206842843961af3c1f7cceae68eef39b70f5
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-12-18 14:59:25 -0800

    Allow `global_map` to store possibly empty values
    Other types like `std::variant` or `std::optional` support emptiness,
    but cannot be constructed from `nullptr`.
    
    This patch allows the two aforementioned types to be used as `value_t`
    for `global_map`.

--- src/utils/global_map.hpp
+++ src/utils/global_map.hpp
@@ -3,13 +3,20 @@
 
 #include <cassert>
 #include <map>
+#include <optional> // nullopt_t
 #include <ostream>
 #include <string>
+#include <variant> // monostate
+
+template<typename dest_t, typename... src_t>
+static constexpr bool can_nothrow_construct_any
+    = (false or ... or std::is_nothrow_constructible_v<dest_t, src_t>);
 
 template<typename key_t, typename value_t>
 class global_map final {
-    static_assert(std::is_nothrow_constructible_v<value_t, std::nullptr_t>,
-                  "global_map can only use pointer-like types for its value_t");
+    static_assert(
+        can_nothrow_construct_any<value_t, std::nullptr_t, std::nullopt_t, std::monostate>,
+        "global_map can only use possibly empty types for `value_t`");
 
   public:
     [[nodiscard]] value_t lookup(const std::string & mod, const key_t & key) const {
@@ -17,10 +24,10 @@ class global_map final {
         if constexpr (std::is_same_v<key_t, std::string>) { assert(not key.empty()); }
 
         auto mod_iter = globals.find(mod);
-        if (mod_iter == globals.end()) { return nullptr; }
+        if (mod_iter == globals.end()) { return empty_value(); }
 
         auto item_iter = mod_iter->second.find(key);
-        return item_iter != mod_iter->second.end() ? item_iter->second : nullptr;
+        return item_iter != mod_iter->second.end() ? item_iter->second : empty_value();
     }
 
     void add(const std::string & mod, const key_t & key, value_t value) {
@@ -31,6 +38,16 @@ class global_map final {
         auto & module_exports = mod_iter->second;
         assert(module_exports.find(key) == module_exports.end());
         module_exports.emplace(key, value);
+    }
+
+    [[nodiscard]] static constexpr value_t empty_value() {
+        if constexpr (std::is_nothrow_constructible_v<value_t, std::nullptr_t>) {
+            return nullptr;
+        } else if constexpr (std::is_nothrow_constructible_v<value_t, std::nullopt_t>) {
+            return std::nullopt;
+        } else if constexpr (std::is_nothrow_constructible_v<value_t, std::monostate>) {
+            return std::monostate{};
+        }
     }
 
   private:
