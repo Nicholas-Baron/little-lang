bee9a8fa8ec47fc58bc71404be41ca3500edded5
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2023-01-03 13:27:59 -0800

    Lower function types into LLVM

--- src/llvm_type_lowering.cpp
+++ src/llvm_type_lowering.cpp
@@ -48,6 +48,22 @@ llvm::Type * llvm_type_lowering::lower_to_llvm(ast::type_ptr type) {
             active_types.emplace(type, llvm_struct_type);
             return llvm_struct_type;
         }
+
+        if (const auto * func_type = dynamic_cast<const ast::function_type *>(type);
+            func_type != nullptr) {
+
+            auto * result_type = lower_to_llvm(func_type->return_type());
+
+            std::vector<llvm::Type *> args;
+            args.reserve(func_type->arg_count());
+            for (auto i = 0U; i < func_type->arg_count(); ++i) {
+                args.emplace_back(lower_to_llvm(func_type->arg(i)));
+            }
+
+            auto * llvm_func_type = llvm::FunctionType::get(result_type, args, false);
+            active_types.emplace(type, llvm_func_type);
+            return llvm_func_type;
+        }
         return nullptr;
     }
     return iter->second;
