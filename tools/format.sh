e997b8fb87a9bd109a0865f8a4fac112b0e0303f
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2023-05-03 19:46:50 -0700

    Add trailing newlines to C++ files

--- tools/format.sh
+++ tools/format.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-clang-format $(find . -type f -name '*.[ch]pp' ! -path './build/*' | tr '\n' ' ') -i -verbose -style=file -fallback-style=none
+files=$(find . -type f -name '*.[ch]pp' ! -path './build/*' | tr '\n' ' ')
+clang-format $files -i -verbose -style=file -fallback-style=none
 cmake-format $(find . -type f -name 'CMakeLists.txt' | tr '\n' ' ') -i -c .cmake-format
+
+for f in $files; do
+	if [ -n "$(tail -c 1 "$f")" ]; then
+		echo >> "$f"
+	fi
+done
 
 git diff
