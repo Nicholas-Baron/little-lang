4fbc38ca58622b5ffac43f06e30363b1b1fd7b84
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2023-06-06 13:58:08 -0700

    Add another test for ast to cfg lowering

--- test/ast_to_cfg_test.cpp
+++ test/ast_to_cfg_test.cpp
@@ -59,6 +59,16 @@ static void scan_graph(const control_flow::function_start * start,
                    struct_init != nullptr) {
             CHECK(struct_init->next != nullptr);
             frontier.push(struct_init->next);
+        } else if (const auto * func_call
+                   = dynamic_cast<const control_flow::function_call *>(visiting);
+                   func_call != nullptr) {
+            CHECK(func_call->next != nullptr);
+            frontier.push(func_call->next);
+        } else if (const auto * member_access
+                   = dynamic_cast<const control_flow::member_access *>(visiting);
+                   member_access != nullptr) {
+            CHECK(member_access->next != nullptr);
+            frontier.push(member_access->next);
         } else {
             std::cout << typeid(*visiting).name() << std::endl;
             assert(false);
@@ -261,6 +271,47 @@ TEST_CASE("ast_to_cfg can lower an intrinsic followed by a return") {
         CHECK(func_start->next != nullptr);
     });
     CHECK(root_count == 1);
+
+    // The last node generated should be a function end.
+    CHECK(cfg->previous_node() != nullptr);
+    auto * func_end = dynamic_cast<control_flow::function_end *>(cfg->previous_node());
+    CHECK(func_end != nullptr);
+
+    scan_graph(start, func_end);
+}
+
+TEST_CASE("ast_to_cfg can lower a function call followed by an intrinsic call") {
+    std::string buffer = "bar { lol : bool } foo() -> bar { return bar {lol = true}; } lol() { let "
+                         "x = foo(); let y = syscall(x.lol); }";
+    std::cout << "Testing " << buffer << std::endl;
+
+    ast::type_context ty_context;
+    auto parser = parser::from_buffer(buffer, ty_context);
+    auto mod = parser->parse();
+
+    auto lowering = ast_to_cfg{ty_context};
+
+    lowering.visit(*mod);
+
+    auto cfg = std::move(lowering).take_cfg();
+    CHECK(cfg != nullptr);
+
+    // There should be 1 root.
+    const control_flow::function_start * start = nullptr;
+    auto root_count = 0U;
+    cfg->for_each_function([&](auto * root) {
+        CHECK(root != nullptr);
+        ++root_count;
+
+        const auto * func_start = dynamic_cast<const control_flow::function_start *>(root);
+        CHECK(func_start != nullptr);
+        start = func_start;
+        CHECK(func_start->arg_count == 0);
+
+        CHECK_FALSE(func_start->exported);
+        CHECK(func_start->next != nullptr);
+    });
+    CHECK(root_count == 2);
 
     // The last node generated should be a function end.
     CHECK(cfg->previous_node() != nullptr);
