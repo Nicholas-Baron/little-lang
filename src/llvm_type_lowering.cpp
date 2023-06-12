d9dc62fca9a7976c3c8cba2c388b35f9508d2ae6
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2023-06-12 14:16:18 -0700

    Add error message to LLVM type lowering and remove 1 indent level

--- src/llvm_type_lowering.cpp
+++ src/llvm_type_lowering.cpp
@@ -1,5 +1,7 @@
 #include "llvm_type_lowering.hpp"
 
+#include <iostream>
+
 #include <llvm/IR/DerivedTypes.h>
 
 llvm_type_lowering::llvm_type_lowering(ast::type_context & type_context,
@@ -20,49 +22,51 @@ llvm_type_lowering::llvm_type_lowering(ast::type_context & type_context,
 }
 
 llvm::Type * llvm_type_lowering::lower_to_llvm(ast::type_ptr type) {
-
-    auto iter = std::find_if(active_types.begin(), active_types.end(),
-                             [&type](const auto & entry) -> bool { return entry.first == type; });
-
-    if (iter == active_types.end()) {
-        if (type->is_pointer_type()) {
-            // Find the pointed-to type.
-            const auto & ast_ptr_type = dynamic_cast<const ast::ptr_type &>(*type);
-            auto * pointed_to_type = lower_to_llvm(ast_ptr_type.pointed_to_type());
-            assert(pointed_to_type != nullptr);
-            return pointed_to_type->getPointerTo();
-        }
-
-        if (const auto * struct_type = dynamic_cast<const ast::struct_type *>(type);
-            struct_type != nullptr) {
-
-            std::vector<llvm::Type *> fields;
-            for (auto i = 0U; i < struct_type->field_count(); ++i) {
-                const auto & [name, type] = struct_type->field(i);
-                fields.emplace_back(lower_to_llvm(type));
-            }
-
-            auto * llvm_struct_type = llvm::StructType::create(fields, struct_type->user_name());
-            active_types.emplace(type, llvm_struct_type);
-            return llvm_struct_type;
-        }
-
-        if (const auto * func_type = dynamic_cast<const ast::function_type *>(type);
-            func_type != nullptr) {
-
-            auto * result_type = lower_to_llvm(func_type->return_type());
-
-            std::vector<llvm::Type *> args;
-            args.reserve(func_type->arg_count());
-            for (auto i = 0U; i < func_type->arg_count(); ++i) {
-                args.emplace_back(lower_to_llvm(func_type->arg(i)));
-            }
-
-            auto * llvm_func_type = llvm::FunctionType::get(result_type, args, false);
-            active_types.emplace(type, llvm_func_type);
-            return llvm_func_type;
-        }
-        return nullptr;
+    if (auto iter
+        = std::find_if(active_types.begin(), active_types.end(),
+                       [&type](const auto & entry) -> bool { return entry.first == type; });
+        iter != active_types.end()) {
+        return iter->second;
     }
-    return iter->second;
+
+    if (type->is_pointer_type()) {
+        // Find the pointed-to type.
+        const auto & ast_ptr_type = dynamic_cast<const ast::ptr_type &>(*type);
+        auto * pointed_to_type = lower_to_llvm(ast_ptr_type.pointed_to_type());
+        assert(pointed_to_type != nullptr);
+        return pointed_to_type->getPointerTo();
+    }
+
+    if (const auto * struct_type = dynamic_cast<const ast::struct_type *>(type);
+        struct_type != nullptr) {
+
+        std::vector<llvm::Type *> fields;
+        for (auto i = 0U; i < struct_type->field_count(); ++i) {
+            const auto & [name, type] = struct_type->field(i);
+            fields.emplace_back(lower_to_llvm(type));
+        }
+
+        auto * llvm_struct_type = llvm::StructType::create(fields, struct_type->user_name());
+        active_types.emplace(type, llvm_struct_type);
+        return llvm_struct_type;
+    }
+
+    if (const auto * func_type = dynamic_cast<const ast::function_type *>(type);
+        func_type != nullptr) {
+
+        auto * result_type = lower_to_llvm(func_type->return_type());
+
+        std::vector<llvm::Type *> args;
+        args.reserve(func_type->arg_count());
+        for (auto i = 0U; i < func_type->arg_count(); ++i) {
+            args.emplace_back(lower_to_llvm(func_type->arg(i)));
+        }
+
+        auto * llvm_func_type = llvm::FunctionType::get(result_type, args, false);
+        active_types.emplace(type, llvm_func_type);
+        return llvm_func_type;
+    }
+
+    std::cerr << "Could not lower " << *type << std::endl;
+    return nullptr;
 }
