b53259dd1c3cf02bd1488e2cf4deada567632a01
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2023-07-02 11:18:09 -0700

    Add script for fuzzing setup

---
+++ tools/fuzz.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+# Setup build
+mkdir -p build
+cd build
+
+cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ ..
+ninja fuzzer
+
+[ -f corpus.txt ] || fd -t f -e lil ../examples/ > corpus.txt
+
+./bin/fuzzer -seed_inputs=@corpus.txt -runs=25 -max_len=1024
+
+mkdir -p crashes
+mv -vt crashes crash-*
+
